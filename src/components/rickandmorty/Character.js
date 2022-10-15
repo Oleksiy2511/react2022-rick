6167637b078a9462d6a5db7e3fed8591c4959097
Oleksiy2511 <kurulenko275@gmail.com>
2022-10-15 18:34:18 +0300

    rick and morty

--- src/components/rickandmorty/Character.js
+++ src/components/rickandmorty/Character.js
@@ -1,8 +1,3 @@
-// 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
-// https://rickandmortyapi.com/
-//     https://rickandmortyapi.com/api/character
-//         Створити 6 персонажів
-
 
 import characters from "./Characters";
 
